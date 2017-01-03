3bc8fffd6460c72add1d16af3f75b45610aeb910
CatWantsMeow <cat.wants.meow@gmail.com>
2017-01-03 20:57:53 +0300

    Fixed account number validation (#71)

--- services/backend/jupiter/apps/finance/api/accounts/serializers.py
+++ services/backend/jupiter/apps/finance/api/accounts/serializers.py
@@ -17,7 +17,7 @@ class AccountSerializer(ModelSerializer):
     def assign_validate(data):
         errors = {}
         number = data['account_number']
-        if re.match('^3014\d{9}', number) is None:
+        if re.match('^3014\d{9}$', number) is None:
             errors['account_number'] = 'Номер расчетного счета для физ. лиц должен иметь формат 3014ЦЦЦЦЦЦЦЦЦ.'
         elif fin_models.Account.get_last_digit(number[:-1]) != number[-1]:
             errors['account_number'] = 'Допущена ошибка при наборе номера. ' \
